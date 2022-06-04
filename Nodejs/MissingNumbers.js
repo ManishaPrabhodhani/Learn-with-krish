abe83938af1a888f66c652893cd52a9e6e70df3f
ManishaPrabhodhani <106021179+ManishaPrabhodhani@users.noreply.github.com>
2022-06-04 11:58:12 +0530

    Finding Missing Numbers

---
+++ Nodejs/MissingNumbers.js
@@ -0,0 +1,15 @@
+var array = [21,25,29,28,22,24,27,26,30],
+arr = array.sort()
+
+
+count = arr[arr.length-1]
+missingNumber = []
+
+for (let i = arr[0]; i <= count; i++) {
+  if (arr.indexOf(i) === -1) {
+    missingNumber.push(i)
+  }
+}
+
+//console.log(array)
+console.log(missingNumber
