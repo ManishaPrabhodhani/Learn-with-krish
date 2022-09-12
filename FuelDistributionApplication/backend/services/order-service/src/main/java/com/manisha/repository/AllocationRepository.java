6a8ed1156c537fe102c2e8e22fd4ba6985a83aba
ManishaPrabhodhani <106021179+ManishaPrabhodhani@users.noreply.github.com>
2022-09-12 06:20:21 -0700

    Update

--- FuelDistributionApplication/backend/services/order-service/src/main/java/com/manisha/repository/AllocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manisha.repository;
-
-import com.manisha.entity.Allocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllocationRepository extends JpaRepository<Allocation,Integer> {
-}
