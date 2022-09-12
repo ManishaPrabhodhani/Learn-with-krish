05321bdefc6754d0d390110e293c99f7e1c5e4b9
ManishaPrabhodhani <106021179+ManishaPrabhodhani@users.noreply.github.com>
2022-09-12 00:37:03 -0700

    Update

--- Fuel Distribution/services/springboot-gas-station-order-service/src/main/java/com/manisha/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manisha.repository;
-
-import com.manisha.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
