5c0dfa9168743a5bd5d1ca318c16643791a9450e
ManishaPrabhodhani <106021179+ManishaPrabhodhani@users.noreply.github.com>
2022-09-12 00:42:30 -0700

    Update

--- Fuel Distribution/backend/services/allocation-service/src/main/java/com/manisha/repository/DispatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manisha.repository;
-
-import com.manisha.entity.Dispatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DispatchRepository extends JpaRepository<Dispatch,Integer> {
-}
--- Fuel Distribution/backend/services/allocation-service/src/main/java/com/manisha/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manisha.repository;
-
-import com.manisha.entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock,Integer> {
-}
