b6c950e3bfe06383fe66984b103ff8cf2de73768
ManishaPrabhodhani <106021179+ManishaPrabhodhani@users.noreply.github.com>
2022-09-14 09:39:36 -0700

    Update Event

--- FuelDistributionApplication/backend/services/allocation-service/src/main/java/com/manisha/event/Event.java
+++ FuelDistributionApplication/backend/services/allocation-service/src/main/java/com/manisha/event/Event.java
@@ -11,11 +11,11 @@ public class Event {
 
     private String serviceName;
     private String Type;
-    private String uniqueId;    //key
+    private String uniqueId;    
     private String fuelQty;
     private String orderid;
     private String stockid;
-    private String outcome;   //result
+    private String outcome;   
 
 
 }
